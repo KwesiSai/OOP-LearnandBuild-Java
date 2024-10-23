33e4ade47362730287356dc10c1dcc1e5447fbc5
KwesiSai <kwesiabraham4503@gmail.com>
2024-10-23 10:24:55 +0000

    Printing numbers without double quotes

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 public class Main {
 	public static void main(String[] args){
 		System.out.println("Hello World");
-        System.out.println("The world dey bee");
-        System.out.print(" It dey bee keke");
+        System.out.print("The world dey bee");
+        System.out.println(" It dey bee keke ");
+        System.out.println(590454);
+
 	}
 }
