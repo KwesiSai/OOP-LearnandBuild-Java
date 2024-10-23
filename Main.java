66c149f023bf0ad8b9461607c4066e76555b6825
KwesiSai <kwesiabraham4503@gmail.com>
2024-10-23 10:43:21 +0000

    More on declaring variables

--- Main.java
+++ Main.java
@@ -5,14 +5,17 @@ public class Main {
         System.out.println(" It dey bee keke ");
         System.out.println(590454);
         System.out.println(100 * 45);
-        String name = "Abraham";
-        int age = 22;
-        char gender = 'M';
-        boolean married = false;
+        String name = "Abraham"; //String type is use for words or group of words and must be put in double quotes
+        int age = 22; //int is used for integers, negative and positive integers
+        double weight = 73.55; //float is used for positive and negative decimals
+        char gender = 'M'; //char is for single letters or characters and must be wrapped in single quotes
+        boolean married = false; // Boolean is for storing boolean states, true or false
         System.out.println(name);
         System.out.println(age);
         System.out.println(gender);
         System.out.println(married);
+        System.out.println(weight);
+        
 	}
     //Ignore this because it is a comment
     /*
