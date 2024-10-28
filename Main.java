a24051ac7fff3bce3034edd49d7dfaffa51e7cf4
KwesiSai <kwesiabraham4503@gmail.com>
2024-10-28 17:33:25 +0000

    Progress: conditional statements, if-else

--- Main.java
+++ Main.java
@@ -1,29 +1,29 @@
 public class Main {
 	public static void main(String[] args){
-		System.out.println("Hello World");
-        System.out.print("The world dey bee");
-        System.out.println(" It dey bee keke ");
-        System.out.println(590454);
-        System.out.println(100 * 45);
-        String name = "Abraham"; //String type is use for words or group of words and must be put in double quotes
-        int age = 22; //int is used for integers, negative and positive integers
-        float weight = 73.55f; //float is used for positive and negative decimals
-        char gender = 'M'; //char is for single letters or characters and must be wrapped in single quotes
-        boolean married = false; // Boolean is for storing boolean states, true or false
-        System.out.println(name);
-        System.out.println(age);
-        System.out.println(gender);
-        System.out.println(married);
-        System.out.println(weight);
-        final int newage = 23;
-        //The final keyword declares a variable as a constant
-        System.out.println(newage);
-        System.out.println("Hey there my dear " + name); //Printing text and variables 
-        String nickname = "Mafia";
-        String userName = name + nickname;
-        String username = name + " " + nickname;
-        System.out.println(username);
-        System.out.println(userName);
+		// System.out.println("Hello World");
+        // System.out.print("The world dey bee");
+        // System.out.println(" It dey bee keke ");
+        // System.out.println(590454);
+        // System.out.println(100 * 45);
+        // String name = "Abraham"; //String type is use for words or group of words and must be put in double quotes
+        // int age = 22; //int is used for integers, negative and positive integers
+        // float weight = 73.55f; //float is used for positive and negative decimals
+        // char gender = 'M'; //char is for single letters or characters and must be wrapped in single quotes
+        // boolean married = false; // Boolean is for storing boolean states, true or false
+        // System.out.println(name);
+        // System.out.println(age);
+        // System.out.println(gender);
+        // System.out.println(married);
+        // System.out.println(weight);
+        // final int newage = 23;
+        // //The final keyword declares a variable as a constant
+        // System.out.println(newage);
+        // System.out.println("Hey there my dear " + name); //Printing text and variables 
+        // String nickname = "Mafia";
+        // String userName = name + nickname;
+        // String username = name + " " + nickname;
+        // System.out.println(username);
+        // System.out.println(userName);
         
         //Variables and identifies
         //real life code to calculate area of a rectangle
@@ -32,6 +32,11 @@ public class Main {
         int area = length * breadth;
 
         System.out.println("The area of the rectangle is: " + area);
+        System.out.println(length < breadth);
+        if(length > breadth){
+            System.out.println("This is truly a rectangle");
+        }
+
 	}
     //Ignore this because it is a comment
     /*
