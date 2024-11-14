31c1e4f07ef75516b779c31d2fba856ac192b55b
KwesiSai <kwesiabraham4503@gmail.com>
2024-11-14 11:38:52 +0000

    Method overloading in Java

--- Main.java
+++ Main.java
@@ -2,12 +2,16 @@
 
 public class Main {
 	public static void main(String[] args){
-        String message = newMethod("Emily"); //calling methods
+        String message = newMethod("Dear", "Emily"); //calling methods
         System.out.println(message);
 
     }
     //Defining methods
     public static String newMethod(String person){
         return "Hey " + person + " we are onto methods now!!!";
+    }
+    //Method overloading
+    public static String newMethod(String greeting, String person){
+        return greeting + " " + person + " we are onto methods now!!!";
     }
 }        
