4246c3e14eb605145d86c5a48eccdff93d345ada
KwesiSai <kwesiabraham4503@gmail.com>
2024-11-15 10:17:46 +0000

    Intro to OOP

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    int x = 15;
+    String car = "Toyota";
+    public static void main(String[] args){
+        Test newObject = new Test();
+        System.out.println(newObject.x);
+        System.out.println(newObject.car);
+    }
+}
