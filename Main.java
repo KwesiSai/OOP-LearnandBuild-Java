8af08907f6aaef4ef496e42a0f387152fd293c0e
KwesiSai <kwesiabraham4503@gmail.com>
2024-11-15 08:57:34 +0000

    Recursion

--- Main.java
+++ Main.java
@@ -5,6 +5,9 @@ public class Main {
         String message = newMethod("Dear", "Emily"); //calling methods
         System.out.println(message);
 
+        int myfactorial = factorial(4);
+        System.out.println(myfactorial);
+
     }
     //Defining methods
     public static String newMethod(String person){
@@ -13,5 +16,17 @@ public class Main {
     //Method overloading
     public static String newMethod(String greeting, String person){
         return greeting + " " + person + " we are onto methods now!!!";
+    }
+    //Using recursion to find factorial of a number
+    public static int factorial (int num){
+        if(num == 1){
+        return 1;
+        }
+        else if(num == 0){
+        return 1;
+        }
+        else{
+            return num * factorial(num - 1);
+        }
     }
 }        
