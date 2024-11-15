7216358fa5cf9d09277f4c192ab9cfe932f2b7aa
KwesiSai <kwesiabraham4503@gmail.com>
2024-11-15 13:45:45 +0000

    Modifying attributes (instance variables)

--- Test.java
+++ Test.java
@@ -3,9 +3,12 @@ public class Test {
     String car = "Toyota";
     public static void main(String[] args){
         Test newObject = new Test();
+        newObject.x = 29;
         System.out.println(newObject.x);
         System.out.println(newObject.car);
         Test secondObject = new Test();
-        System.out.println(secondObject.car);
+        System.out.println(secondObject.x);
+    
+        
     }
 }
