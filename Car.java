f8782637989a4b1d714c028be7e2dc85d9cc2367
KwesiSai <kwesiabraham4503@gmail.com>
2024-11-18 09:21:05 +0000

    Test: Defining two public classes in one file - Error

--- Car.java
+++ Car.java
@@ -8,7 +8,14 @@ public class Test {
         System.out.println(newObject.car);
         Test secondObject = new Test();
         System.out.println(secondObject.x);
+        Car newCar = new Car();
+        newCar.throttle();
     
         
+    }
+}
+public class Car{
+    public void throttle(){
+        System.out.println("This if full throttle");
     }
 }
