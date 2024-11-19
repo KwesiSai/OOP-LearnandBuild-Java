aa47aea1aae773f81f81a9a22a62fa4a2f07187b
KwesiSai <kwesiabraham4503@gmail.com>
2024-11-19 13:42:27 +0000

    Implementing OOP principles - Abstraction_Encapsulation_Polymorphism_Inheritance

--- Test.java
+++ Test.java
@@ -4,9 +4,9 @@ public class Test {
     String name;
 
     //Constructor
-    public Test(String name, int code){
-        this.name = name;
-        this.code = code;
+    public Test(String course, int courseCode){
+        this.name = course;
+        this.code = courseCode;
     }
     //class method
     public void display(){
@@ -17,6 +17,34 @@ public class Test {
     public static void main(String[] args){
         Test newTest = new Test("English", 101);
         newTest.display();
+        fece objfac = new fece();
+        objfac.facultyData();
         
+    }
+}
+
+abstract class Faculty {
+    private String collegeName = "Engineering";
+    private int collegeCode = 140;
+
+    abstract void facultyData();
+    
+    String getCollegeName() {
+        return collegeName;
+    }
+    int getCollegeCode(){
+        return collegeCode;
+    }
+}
+
+class fece extends Faculty {
+    String facultyName = "Electrical";
+    int facultyCode = 505;
+
+    void facultyData() {
+        System.out.println(getCollegeName());
+        System.out.println(getCollegeCode());
+        System.out.println(facultyName);
+        System.out.println(facultyCode);
     }
 }
